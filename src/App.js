ebacf2387fa00ef5a64579f55e99975c24dc718f
George Zhang <zhanggq1996@gmail.com>
2023-07-23 18:26:30 -0400

    redesign sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { filterItems } from "./utils/searchbar_utils";
+import { SearchBar } from "./utils/searchbar_utils";
+// import { Icon } from "leaflet";
+
 import "./App.css";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
-import { filterItems } from "./utils/searchbar_utils";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
@@ -53,11 +55,17 @@ function MapPageLayout({ locationData, setlocationData }) {
   return (
     <div className="bg-gray-950 flex justify-center items-center min-h-screen p-10">
       <div className="canvas flex">
-        <div id="map" className="leaflet-container w-3/4">
+        <div id="map" className="leaflet-container w-3/4 h-full">
           <BasicMap></BasicMap>
         </div>
-        <div className="filterablelist w-1/4">
-          <FilterableList items={locationData}></FilterableList>
+
+        <div className="inline-block h-full mx-4 w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
+
+        <div className="filterablelist w-1/4 h-full">
+          <FilterableList
+            className="h-full"
+            items={locationData}
+          ></FilterableList>
         </div>
       </div>
     </div>
@@ -83,7 +91,7 @@ function BasicMap( { locations, center } ) {
       </Marker>
       ) 
        */}
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[43.6226 + 0.05, -79.45 + 0.05]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
@@ -112,70 +120,117 @@ function FilterableList( { items } ) {
   }
 
   return (
-    <>
-      <div className="bg-gray-600 bg-opacity-20 border border-gray-200 rounded-md">
-        <SearchBar query={query} onChange={handleChange} />
+    // This nested div is to make the overflow + top align work properly.
+    // Without the nested div, either (1) overflow but not top align, or
+    // (2) top align but not overflow.
+    // See: https://chat.openai.com/c/e52a3b12-f47d-443e-aef6-5f1b3b76783a
+    // <div className="pl-4 container mx-auto h-full rounded-lg grid place-items-start overflow-y-auto flex ">
+    //   <div className="flex-1 grid place-items-center overflow-y-auto w-full">
+    //     <div className="w-full">
+    //       <SearchBar className="w-full" query={query} onChange={handleChange} />
+    //     </div>
+    //     {/* <hr /> */}
+    //     <div className="w-full">
+    //       <LocationList items={results} />
+    //     </div>
+    //   </div>
+    // </div>
+
+    // Removed overflow-y-auto from this one so it has no scrollwheel
+    // Added h-full so it takes space of parent container
+
+    // EDIT: Had issue where doing h-full would make this 640px, height of parent, but
+    // don't want that since searchbar above it takes up some space already.
+    // Soln was: https://stackoverflow.com/questions/64257049/how-to-fill-up-the-rest-of-the-screen-height-using-tailwindcss
+    // tldr: Put className="flex flex-col" in outer with searchbar
+    // className="overflow-y-auto flex-grow" in inner
+    // outer takes height of parent
+    // inner uses flex-grow to fill remaining space
+    <div className="flex flex-col h-full w-full">
+      <SearchBar className="w-full" query={query} onChange={handleChange} />
+      <div className="mx-0 rounded-lg place-items-start overflow-y-auto flex-grow">
+        {/* <div className="place-items-center overflow-y-auto w-full h-full">
+          <LocationList items={results} />
+        </div> */}
+        <LocationList items={results} />
       </div>
-      <hr />
-      <LocationList items={results} />
-    </>
+    </div>
   );
 }
 
-function SearchBar({ query, onChange }) {
-  return (
-    <label>
-      Search: <input value={query} onChange={onChange} />
-    </label>
-  );
-}
-
-function LocationList({ items }) {
-  const spliced_items = items.slice(0, 5);
-  return (
-    <table>
-      <tbody>
-        {spliced_items.map((item) => (
-          <tr key={item.attributes.opendata_id}>
-            <td>{item.attributes.date_updated}</td>
-            <td>{item.attributes.geojson.location}</td>
-            {/* <td>{item.geojson.geometry}</td> */}
-            <td>{item.attributes.geojson.geometry.coordinates[0].toFixed(2)}</td>
-            <td>{item.attributes.geojson.geometry.coordinates[1].toFixed(2)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
-
-// Get data
-
-// function getData() {
-//   // https://levelup.gitconnected.com/3-ways-to-make-an-api-call-in-react-3734d025d92
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     fetch("http://35.183.91.143/api/washroomdata/?format=json")
-//       .then((response) => response.json())
-//       .then((json) => setData(json))
-//       .catch((error) => console.error(error));
-//   }, []);
-
+// function SearchBar({ query, onChange }) {
 //   return (
-//     <div>
-//       {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "Loading..."}
+//     <label className="w-full">
+//       Search: <input value={query} onChange={onChange} />
+//     </label>
+//   );
+// }
+
+// Template: https://transmit.tailwindui.com/
+function LocationList({ items }) {
+  const spliced_items = items.slice(0, 10);
+  return (
+    <div className="py-1 basicmap-font">
+      {spliced_items.map((item) => (
+        <div
+          key={item.attributes.opendata_id}
+          className="mb-4 flex flex-col items-start basicmap-item-box"
+        >
+          <h2 id="episode-5-title" className="text-lg font-bold text-slate-900">
+            <a href="/5">{item.attributes.geojson.location}</a>
+          </h2>
+          <p className="mt-1 text-base leading-5 text-slate-700">
+            {item.attributes.geojson.type}
+          </p>
+          {/* <time
+            dateTime="2022-02-24T00:00:00.000Z"
+            className="font-mono text-sm leading-7 text-slate-500"
+          >
+            Last Updated: {item.attributes.date_updated}
+          </time> */}
+          <p className="mt-1 font-mono text-xs leading-7 text-slate-500 leading-normal">
+            Address: {item.attributes.geojson.address}
+          </p>
+          <p className="font-mono text-xs leading-7 text-slate-500 leading-normal">
+            {item.attributes.geojson.location_details}
+          </p>
+          <div className="flex items-center gap-4">
+            <button
+              type="button"
+              className="flex items-center text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900"
+              aria-label={item.attributes.geojson.type}
+            >
+              <svg
+                aria-hidden="true"
+                viewBox="0 0 10 10"
+                fill="none"
+                className="h-2.5 w-2.5 fill-current"
+              >
+                <path d="M8.25 4.567a.5.5 0 0 1 0 .866l-7.5 4.33A.5.5 0 0 1 0 9.33V.67A.5.5 0 0 1 .75.237l7.5 4.33Z"></path>
+              </svg>
+              <span className="ml-3" aria-hidden="true">
+                Additional Information
+              </span>
+            </button>
+            <span
+              aria-hidden="true"
+              className="text-sm font-bold text-slate-400"
+            ></span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+// const LocationlistFrame = ({ children }) => {
+//   return (
+//     <div className="w-full h-full items-center justify-center">
+//       <div className="w-full h-full overflow-y-auto">
+//         {children}
+//       </div>
 //     </div>
 //   );
-// }
-
-// function dataDisplay() {
-//   return (
-//     <tr key={food.id}>
-//       <td>{food.name}</td>
-//       <td>{food.description}</td>
-//     </tr>
-//   );
-// }
+// };
 
 export default App;
--- src/utils/searchbar_utils.js
+++ src/utils/searchbar_utils.js
@@ -7,28 +7,6 @@
 //   );
 // }
 
-// export const foods = [{
-//   id: 0,
-//   name: 'Sushi',
-//   description: 'Sushi is a traditional Japanese dish of prepared vinegared rice'
-// }, {
-//   id: 1,
-//   name: 'Dal',
-//   description: 'The most common way of preparing dal is in the form of a soup to which onions, tomatoes and various spices may be added'
-// }, {
-//   id: 2,
-//   name: 'Pierogi',
-//   description: 'Pierogi are filled dumplings made by wrapping unleavened dough around a savoury or sweet filling and cooking in boiling water'
-// }, {
-//   id: 3,
-//   name: 'Shish kebab',
-//   description: 'Shish kebab is a popular meal of skewered and grilled cubes of meat.'
-// }, {
-//   id: 4,
-//   name: 'Dim sum',
-//   description: 'Dim sum is a large range of small dishes that Cantonese people traditionally enjoy in restaurants for breakfast and lunch'
-// }];
-
 // ChatGPT:
 // This modified function will return a new array that contains 
 // only the elements from the items array where every word in the 
@@ -43,4 +21,120 @@ export function filterItems(items, query) {
         .some((word) => word.toLowerCase().startsWith(queryWord))
     )
   );
+}
+
+export function SearchBar({ query, onChange }) {
+  return (
+    <form>
+      <div className="flex basicmap-font">
+        <label
+          htmlFor="search-dropdown"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Your Email
+        </label>
+        {/* Removed dropdown */}
+        <div className="relative w-full">
+          <input
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-pink-500 focus:border-pink-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-pink-500"
+            placeholder="Search Locations"
+            value={query}
+            onChange={onChange}
+          />
+          <button
+            type="submit"
+            className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-pink-500 rounded-r-lg border border-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+
+{
+  /* <button
+  id="dropdown-button"
+  data-dropdown-toggle="dropdown"
+  class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+  type="button"
+>
+  All categories{" "}
+  <svg
+    class="w-2.5 h-2.5 ml-2.5"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 10 6"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="m1 1 4 4 4-4"
+    />
+  </svg>
+</button> 
+<div
+  id="dropdown"
+  class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+>
+  <ul
+    class="py-2 text-sm text-gray-700 dark:text-gray-200"
+    aria-labelledby="dropdown-button"
+  >
+    <li>
+      <button
+        type="button"
+        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+      >
+        Mockups
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+      >
+        Templates
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+      >
+        Design
+      </button>
+    </li>
+    <li>
+      <button
+        type="button"
+        class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+      >
+        Logos
+      </button>
+    </li>
+  </ul>
+</div> */
 }
